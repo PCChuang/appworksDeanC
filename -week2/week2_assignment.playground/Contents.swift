b0dc575c1f549a5905cabae530ec96bde798111e
PCC <juangpochieh@gmail.com>
2021-08-08 00:35:19 +0800

    Adding week2 assignment

---
+++ -week2/week2_assignment.playground/Contents.swift
@@ -0,0 +1,193 @@
+// Object-Oriented Swift
+
+// 1.2.3.
+class Animal {
+    enum Gender {
+        case male
+        case female
+        case undefined
+    }
+    func eat() {
+        print("I eat everything!")
+    }
+}
+
+class Elephant: Animal {
+    override func eat() {
+        print("I eat grass!")
+    }
+}
+
+class Tiger: Animal {
+    override func eat() {
+        print("I eat animals!")
+    }
+}
+
+class Horse: Animal {
+    override func eat() {
+        print("I eat hay!")
+    }
+}
+
+class Zoo {
+    var weeklyHot: Animal
+    init(weeklyHot: Animal) {
+        self.weeklyHot = weeklyHot
+    }
+}
+
+let tiger = Tiger()
+let elephant = Elephant()
+let horse = Horse()
+
+let zoo = Zoo(weeklyHot: Tiger())
+
+
+zoo.weeklyHot = tiger
+zoo.weeklyHot = elephant
+zoo.weeklyHot = horse
+
+/*
+ 
+ 4.
+ - Structures are values types, while classes are reference types.
+ - Classes can inherit methods, properties, and other characteristics from other classes, while structures can't.
+ - An instance of a structure is seen as a value, while class instance is object with identity.
+ - Structures copy values when used in the new place, while classes share their data.
+ - Structures are immutable when declared as constants, while classes properties remain mutable.
+ 
+ 5.
+ Instance methods are methods which we call on an instance of a particaular type, and they can't be called whithout an existing instance.
+ On the other hand, type methods are called on the type itself, and it can still be called without an existing instance of that type.
+ 
+ 6.
+ Intitalizers are called to create an instance of a particular type.
+ They allow us to proceed with initialization, ensuring every stored property of the instance has a intial value,
+ which is required in a class or a structure.
+ 
+ 7.
+ - in instance method: self is equivalent to the instance itself. We can use it to refer to current instance within the instance method.
+ - in type method: self is equivalent to the type itself. We can use it to tell apart type property and type method parameter from another.
+ 
+ 8.
+ - Reference types share a single copy of data, while value types keep a unique copy of data.
+ - For value types, we can't mutate the instance when declared as constants, regardless of whether properties are declared with let or var.
+   But for reference type, we can mutate the instace itself.
+ 
+ 
+ */
+
+// Enumerations and Optionals in Swift
+
+// 1.
+enum Gasoline: String {
+    case oil92 = "92"
+    case oil95 = "95"
+    case oil98 = "98"
+    case diesel = "diesel"
+
+    func getPrice(for gasoline: Gasoline) -> Double {
+        switch gasoline {
+        case .oil92:
+            return 28.7
+        case .oil95:
+            return 30.2
+        case .oil98:
+            return 32.3
+        case .diesel:
+            return 25.8
+        }
+    }
+}
+
+Gasoline.diesel.rawValue
+
+/*
+ Associtate values are the information we can use to store values of other types alongside the defined case values.
+ - associate values can have different types for each case
+ - we can add label names to associate values
+ - we can only use either raw value or associate value in an enumeration, not both
+ 
+ */
+
+// 2.
+
+class Pet {
+    let name = "Alpha"
+}
+
+class People {
+    var pet: Pet?
+}
+
+func unWrapPetName() {
+    let dean = People()
+    guard let petName = dean.pet?.name
+    else {
+        return
+    }
+}
+
+func unWrapPetName2() {
+    let chris = People()
+    if let petName2 = chris.pet?.name {
+        return
+    }
+}
+
+// Protocol in Swift
+
+// 1.
+
+protocol PoliceMan {
+    func arrestCiminals()
+}
+
+protocol ToolMan {
+    func fixComputer()
+}
+
+struct Person: PoliceMan {
+    let name: String
+    let toolMan: ToolMan
+    func arrestCiminals() {
+    }
+}
+
+struct Engineer: ToolMan {
+    let name: String
+    func fixComputer() {
+    }
+}
+
+struct personInstance: ToolMan {
+    func fixComputer() {
+    }
+    func createInstance() {
+        let steven = Person(name: "Steven", toolMan: self)
+    }
+}
+
+// Error Handling in Swift
+
+enum GuessNumberGameError: Error {
+    case wrongNumber
+}
+class GuessNumerGame {
+    var targetNumber = 10
+    func guess(number: Int) throws {
+        do {
+            guard number == targetNumber else {
+                throw GuessNumberGameError.wrongNumber
+            }
+        }
+        catch {
+            print("Guess the right number: \(targetNumber)")
+        }
+    }
+}
+
+try GuessNumerGame().guess(number:20)
+
+
---
+++ -week2/week2_assignment_xcode/week2_assignment/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  week2_assignment
+//
+//  Created by PoChieh Chuang on 2021/8/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactTextView: UILabel!
+    
+    
+    @IBAction func showFunFact(_ sender: UIButton) {
+        
+        let funFacts: [String] = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
+        "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+        "Richard McClintock, a Latin professor at Hampden-Sydney College in ",
+        "looked up one of the more obscure Latin words, consectetur",
+        "from a Lorem Ipsum passage, and going through the cities of the word",
+        "This book is a treatise on the theory of ethics, very popular during the.",
+        "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.."
+        ]
+        
+        let randomInt = Int.random(in: 0...6)
+        
+        let randomfunFacts = funFacts[randomInt]
+        
+        funFactTextView.text = randomfunFacts
+        
+        let backgroundColor: [UIColor] = [
+            .black,
+            .blue,
+            .brown,
+            .cyan,
+            .lightGray,
+            .red,
+            .darkGray
+        ]
+        
+        let randomBackgorundColor = backgroundColor[randomInt]
+        
+        view.backgroundColor = randomBackgorundColor
+        
+        }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
